dbad5c35d2981a342c857094abb43f19862fa1e9
renato salvatori <renato.s@cost.it>
2021-12-06 16:19:08 +0100

    aggiornato packege.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
+const PORT = 80;
 
 const fs = require("fs");
 const path = require("path");
@@ -11,7 +11,7 @@ const getResources = () => JSON.parse(fs.readFileSync(pathToFile))
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello World")
+  res.send("Hello World Api")
 })
 
 app.get("/api/resources/:id", (req, res) => {
